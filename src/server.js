1608d3c8a27cd323734996e6035cd5ba96ddab8a
Eric Johnson <eric.d.johnson@gmail.com>
2020-03-22 14:39:05 -0500

    adding color-picker to choose color of rock

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express();
 const port = 3000;
-var exec = require('child_process').exec
 
 const spawn = require('child_process').spawn;
+const pythonProcess = spawn('node ./index.js');
 
 app.get('/', (req, res) => {
-  exec('node ./index.js');
   res.send('Hello word');
 });
+
+app.get('/color', (req, res) => {
+  pythonProcess.stdin.write("155,88,181\n");
+  res.send('Color set to:'+req);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
