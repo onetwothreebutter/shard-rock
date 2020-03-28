babd67158784bcbd794a80743042c7c0487f7fdc
Eric Johnson <eric.d.johnson@gmail.com>
2020-03-27 20:41:27 -0500

    use query string param for setting color

--- src/server.js
+++ src/server.js
@@ -3,15 +3,17 @@ const app = express();
 const port = 3000;
 
 const spawn = require('child_process').spawn;
+const pythonProcess = spawn('node', [`${__dirname}/index.js`]);
 
 app.get('/', (req, res) => {
   res.send('Hello word');
+
+  if (req.query.color) {
+    pythonProcess.stdin.write('${req.query.color}\n');
+    res.send(`Color set to ${req.query.color}`);
+  }
+
 });
 
-app.get('/color', (req, res) => {
-  const pythonProcess = spawn('node', [`${__dirname}/index.js`]);
-  pythonProcess.stdin.write("155,88,181\n");
-  res.send('Color set to:'+JSON.stringify(req));
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
