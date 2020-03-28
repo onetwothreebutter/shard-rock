ac02b8eda693176c600398a542056b05a6f2c058
Eric Johnson <eric.d.johnson@gmail.com>
2020-03-27 20:32:10 -0500

    update path for index.js

--- src/server.js
+++ src/server.js
@@ -9,9 +9,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/color', (req, res) => {
-  const pythonProcess = spawn('node' ['./index.js']);
+  const pythonProcess = spawn('node', [`${__dirname}/index.js`]);
   pythonProcess.stdin.write("155,88,181\n");
-  res.send('Color set to:'+req);
+  res.send('Color set to:'+JSON.stringify(req));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
