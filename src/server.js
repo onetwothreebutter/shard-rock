ce224fc6bb9e80bb1b5f80ae46546000f53c970c
Eric Johnson <eric.d.johnson@gmail.com>
2020-03-28 10:19:31 -0500

    serve html files

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 const startLEDs = require('./neopix');
 
+const pythonProcess = startLEDs();
 
 app.get('/', (req, res) => {
 
   if (req.query.color) {
-    const pythonProcess = startLEDs();
     pythonProcess.stdin.write(`${req.query.color}\n`);
     res.send(`Color set to ${req.query.color}`);
   } else {
@@ -16,5 +16,6 @@ app.get('/', (req, res) => {
 
 });
 
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
