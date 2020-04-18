7bfe576bbc42e3cc3d6459dfbbc963d66f2c191a
Eric Johnson <eric.d.johnson@gmail.com>
2020-04-18 14:10:20 -0500

    configure express to parse json

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 const port = 3434;
 const {
   pulse, startLEDs, setColor, setPixel, setBrightness, twinkle, submitCommands,
